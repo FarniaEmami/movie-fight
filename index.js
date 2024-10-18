36d8505249a6aba0cbdb70bcc4cee44cc9d65b5e
Farnia Emani <farnia.emami@babelforce.com>
2024-10-18 13:34:38 +0200

    call omdb with search term for auto complete
    Signed-off-by: Farnia Emani <farnia.emami@babelforce.com>

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-console.log('Hi there!');
+const fetchData = async (searchTerm) => {
+    const response = await axios.get('http://www.omdbapi.com/', {
+        params: {
+            apikey: 'b063078d', 
+            s: searchTerm
+        }
+    })
+
+    console.log(response.data)
+}
+
+const input = document.querySelector('input')
+
+const onInput = debounce(event => {
+    fetchData(event.target.value)
+}, 500)
+
+input.addEventListener('input', onInput)
+
+
