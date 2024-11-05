381f2d6f02c8cd70cffa4cfe68e5f91feb453380
Farnia Emani <farnia.emami@babelforce.com>
2024-11-05 17:44:02 +0100

    Add drop-down with movies
    Signed-off-by: Farnia Emani <farnia.emami@babelforce.com>

--- index.js
+++ index.js
@@ -13,24 +13,52 @@ const fetchData = async (searchTerm) => {
     return response.data.Search
 }
 
+const root = document.querySelector('.autocomplete')
+root.innerHTML = `
+    <label><b>Search for a movie</b></label>
+    <input class="input" />
+    <div class="dropdown">
+        <div class="dropdown-menu">
+            <div class="dropdown-content results"></div>
+        </div> 
+    </div>
+`
+
 const input = document.querySelector('input')
+const dropdown = document.querySelector('.dropdown')
+const resultWrapper = document.querySelector('.results')
 
 const onInput = debounce( async event => {
     const movies = await fetchData(event.target.value)
 
-    for(let movie of movies){
-        const div = document.createElement('div')
+    if(!movies.length){
+        dropdown.classList.remove('is-active')
+        return
+    }
 
-        div.innerHTML = `
-            <img src="${movie.Poster}" />
-            <h1>${movie.Title}<h1>
+    resultWrapper.innerHTML = ''
+    dropdown.classList.add('is-active')
+    for(let movie of movies){
+        const option = document.createElement('a')
+        const imgSrc = movie.Poster === 'N/A' ? '' : movie.Poster
+
+        option.classList.add('dropdown-item')
+        option.innerHTML = `
+            <img src="${imgSrc}" />
+            ${movie.Title}
         `
 
-        document.querySelector('#target').appendChild(div)
+        resultWrapper.appendChild(option)
     }
     
 }, 500)
 
 input.addEventListener('input', onInput)
+
+document.addEventListener('click', event => {
+    if(!root.contains(event.target)){
+        dropdown.classList.remove('is-active')
+    }
+})
 
 
